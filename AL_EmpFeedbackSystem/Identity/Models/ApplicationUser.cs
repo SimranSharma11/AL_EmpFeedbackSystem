27be9fca653f87f031d73865acbc42270ab17e17
Anmol Titoriya <Anmol.Titoriya@acelucid.com>
2024-11-23 14:34:06 +0530

    Updated table

--- AL_EmpFeedbackSystem/Identity/Models/ApplicationUser.cs
+++ AL_EmpFeedbackSystem/Identity/Models/ApplicationUser.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace AL_EmpFeedbackSystem.Identity.Models
 {
@@ -6,7 +7,13 @@ namespace AL_EmpFeedbackSystem.Identity.Models
     {
         public string FullName { get; set; }
         public bool ActiveStatus { get; set; }
+        public DateTime DataOfBirth { get; set; }
+        [MaxLength(250)]
+        public string Address { get; set; }
+        public int PostalCode { get; set; }
         public string CreatedBy { get; set; }
         public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public int? UpdatedBy { get; set; }
     }
 }
